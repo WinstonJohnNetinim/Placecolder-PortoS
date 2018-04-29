47a7a0512fb636192e4f8be8225c6348d2128d3d
Gildevan <33264837+Gildevan@users.noreply.github.com>
2018-04-29 03:03:08 -0300

    script da camera

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	public Transform player;
+	public Vector3 offset;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		transform.position = new Vector3 (player.transform.position.x, 0, 0) + offset;
+	}
+}
+
+//	void Update ()
+//{
+//	transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z) + offset;
+//}
+//}
